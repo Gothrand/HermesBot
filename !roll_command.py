d1950800b4da926ddeaf0287837100f5b6856345
duskerop <jdlundien@gmail.com>
2021-05-21 13:47:13 -0500

    all features are working, just need to output information

--- !roll_command.py
+++ !roll_command.py
@@ -14,8 +14,18 @@ skill_dict = {"acrobatics": "Dex",
         "persuasion": "Cha",
         "religion": "Int",
         "sleight": "Dex",
+        "sleightofhand": "Dex",
+        "hand": "Dex",
         "stealth": "Dex",
         "survival": "Wis"
+    }
+
+attrbutes = {"Strength": "str",
+        "Dexterity": "dex",
+        "Constitution": "con",
+        "Intelligence": "int",
+        "Wisdom": "wis",
+        "Charisma": "cha"
     }
 import random
 import discord
@@ -33,6 +43,7 @@ async def roll(ctx, *arg):
     temp_list1 = []
     temp_list2 = []
     final_list = []
+    result = []
 
     if any(map(str.isdigit, arg[0])): # if arg0 contains an int, assume dice roller only
         if "d" not in arg[0]: #checks for multiple sets
@@ -56,20 +67,32 @@ async def roll(ctx, *arg):
             else:
                 mod += int(i)
 
-    elif arg[0].lower() == "save": # checks if saving throw
+    elif arg[0].lower() == "save" or arg[1].lower() == "save": # checks if saving throw
         dice.append(20)
-        for i in [Str, Dex, Con, Int, Wis]
-        attr = "attr" + arg[1].capitalize
-        stat = int(getInfo(player, character, attr))
-        if arg[0].capitalize in getInfo(player, character, "savingThrows").capitalize:
-            mod += int(getInfo(player, character, "profBonus"))
+        for i in attrbutes:
+            if (
+                (arg[0].lower() in i.lower()) or 
+                (arg[1].lower() in i.lower()) or
+                (arg[0].lower() in attrbutes[i]) or 
+                (arg[1].lower() in attrbutes[i])
+                ):
+                attr = "ST " + i
+                mod += int(getInfo(player, character, attr))
 
-    elif arg[0].lower() in skill_dict: # checks if skill check
+    elif arg[0].capitalize() in attrbutes or arg[0].capitalize() in ["Str", "Dex", "Con", "Int", "Wis", "Cha"]: # if straight stat roll
+        if arg[0].capitalize() in attrbutes:
+            attr = attrbutes[arg[0].capitalize()]
+        else:
+            attr = arg[0].capitalize()
+        mod += int(getInfo(player, character, attr))
+
+    elif arg[0].capitalize() in skill_dict.capitalize(): # checks if skill check
         dice.append(20)
-        attr = "attr" + skill_dict[arg[0]]
-        stat = int(getInfo(player, character, attr))
-        if arg[0].lower() in getInfo(player, character, "profList"):
-            mod += int(getInfo(player, character, "profBonus"))
+        if arg[0].lower in ["sleight", "sleightofhand", "hand"]:
+            attr = "SleightofHand"
+        else:
+            attr = arg[0].capitalize()
+        mod += int(getInfo(player, character, attr))
 
     else: #error message if none of previous statments are met
         await ctx.send("word not found")
@@ -80,14 +103,14 @@ async def roll(ctx, *arg):
         if i.lower() == "dis":
             adv -= 1
 
-    for i in range(loop):
+    for i in range(loop): #this loop rolls the dice and sorts them from largest to smallest
         for j in dice:
             temp_list1.append(random.randint(1,j))
             temp_list1.sort(reverse = True)
             if adv != 0:
                 temp_list2.append(random.randint(1,j))
                 temp_list2.sort(reverse = True)
-        if adv != 0:
+        if adv != 0:   #if adv or dis, puts the smaller sum first
             if sum(temp_list1) <= sum(temp_list2):
                 final_list.append(temp_list1)
                 final_list.append(temp_list2)
@@ -97,13 +120,17 @@ async def roll(ctx, *arg):
         else:
             final_list.append(temp_list1)
     
+    for i in range(len(final_list)):
+        if k_kl > 0:
+            temp = final_list[i][:k_kl]
+        elif k_kl < 0:
+            temp = final_list[i][k_kl:]
+        else:
+            temp = final_list[i]
+        result.append(sum(temp) + mod)
     
         
             
-
-
-
-    mod += (stat//2) - 5
     
     output = random.randint(1,dice) + mod # actual dice roller
     await ctx.send(output)
