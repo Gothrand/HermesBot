828f74e58d0af397d0252d6e85db515b156e8b53
rtmfgf <rtmfgf@umsystem.edu>
2021-11-04 13:21:39 -0500

    Added function for custom hermes help command

--- hermesBot.py
+++ hermesBot.py
@@ -1,28 +1,25 @@
 # bot.py
 import os, discord
-from dotenv import load_dotenv
-
-# Command stuff from the discord library
 from discord.ext import commands
 
+from dotenv import load_dotenv
+
+class MyHelpCommand(commands.HelpCommand):
+    async def command_callback(self, ctx, *, command=None):
+        if command:
+           await ctx.send(f"This the help page for the command {command} ")
+        else:
+            await ctx.send("This is the front page for the bots help command")
+
+#TODO:  Convert entire structure of database for bot to postgreSQL from JSON since JSON is not ideal for
+# database management.
 # Load data from a .env file into memory instead of storing it in the bot file itself
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 # Determines what key character the bot uses to detect commands
 bot = commands.Bot(command_prefix='!', description='A Dungeons & Dragons Discord Interface.')
-
-# cog locations (folder.python file name)
-initial_extensions = ['cogs.character',
-                      'cogs.cards',
-                      'cogs.maps',
-                      'cogs.admin',
-                      'cogs.lookups']
-
-# load cogs
-if __name__ == "__main__":
-    for extension in initial_extensions:
-        bot.load_extension(extension)
+bot.help_command = MyHelpCommand()
 
 # needs to be moved to a dedicated cog but is fine here for now.
 @bot.command(name='echo', help='Echos the passed argument.')
@@ -38,6 +35,20 @@ async def on_ready():
 
     await bot.change_presence(status=discord.Status.online, activity=discord.Game("Dungeons & Dragons"))
     print(f'Successfully logged in!')
+
+# cog locations (folder.python file name)
+initial_extensions = ['cogs.character',
+                      'cogs.cards',
+                      'cogs.maps',
+                      'cogs.admin',
+                      'cogs.lookups']
+
+def main():
+    for extension in initial_extensions:
+            bot.load_extension(extension)
+# load cogs
+if __name__ == "__main__":
+    main()
 
 # gogogo
 bot.run(TOKEN)
